dd66f8ef0b21d527e70da011e80b18fa8686462c
hamsty <tallesdesousacosta@gmail.com>
2021-09-15 21:21:53 -0300

    BASIC, ADVANCED and BONUS 2 implemented

--- qapp/queue_client.py
+++ qapp/queue_client.py
@@ -2,9 +2,7 @@ from twisted.internet import reactor
 from twisted.internet.endpoints import clientFromString, connectProtocol
 from client.Communicate import Communicate
 
-
 if __name__ == '__main__':
-
     point = clientFromString(reactor, "tcp:host=172.16.238.10:port=5678:timeout=10")
     connectProtocol(point, Communicate())
     reactor.run()
